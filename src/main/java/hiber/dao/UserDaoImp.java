b77498d72cf5780373c8b3bdc3b2247ce8e563c2
Mustafetov Beknur <mustafetovbr@gmail.com>
2020-11-18 17:57:02 +0300

    fixed

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -34,7 +34,7 @@ public class UserDaoImp implements UserDao {
 
    @Override
    public List<User> getUserByCar(String model,int series){
-      String hql = "select car.user from Car car where car.model =:model and car.series =:series";
+      String hql = "select user from User user where user.car.model =:model and user.car.series =:series";
       TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery(hql).setParameter("series",series).setParameter("model", model);
       return query.getResultList();
    }
